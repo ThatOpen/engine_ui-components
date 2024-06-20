f36705ba351ebad93a4c33c1c42f7b5067458af4
Juan Hoyos <juan.hoyos4@gmail.com>
2024-06-20 15:32:07 -0500

    feat(obc): adds Relations Tree tutorial

--- packages/obc/src/components/tables/RelationsTree/example.ts
+++ packages/obc/src/components/tables/RelationsTree/example.ts
@@ -38,21 +38,50 @@ const highlighter = components.get(OBF.Highlighter);
 highlighter.setup({ world });
 highlighter.zoomToSelection = true;
 
-const indexer = components.get(OBC.IfcRelationsIndexer);
-
 const fragmentsManager = components.get(OBC.FragmentsManager);
 fragmentsManager.onFragmentsLoaded.add(async (model) => {
-  if (model.hasProperties) await indexer.process(model);
   if (world.scene) world.scene.three.add(model);
 });
+
+/* MD 
+  ## Showing your model tree 🌲
+  ---
+  Among the most common things to do with BIM models, is to show their spatial structure. This is really important, because then you can know better the model and see how elements are hierarchized between them. 🔗
+  
+  However, the IFC schema is not always that intuitive when it comes to create a model tree. Why? You may wonder... and it's because IfcRelContainedInSpatialStructure is not the only IFC relation that takes play in a real model tree. 🤯 Luckily, this is already taken into account by That Open Engine's UI when creating a model tree. Let's learn how you can use the Relations Tree!
+
+  ### Creating the tree
+  Before using the Relations Tree, you must have the relations of your model indexed. This will ensure the Relations Tree have access to the required information to create the tree. 🌲
+  */
+
+const indexer = components.get(OBC.IfcRelationsIndexer);
+
+fragmentsManager.onFragmentsLoaded.add(async (model) => {
+  if (model.hasProperties) await indexer.process(model);
+});
+
+/* MD 
+  :::tip
+
+  If you're unsure about how the indexer works, you can take a look at the corresponding [IfcRelationsIndexer](https://docs.thatopen.com/Tutorials/Components/Core/IfcRelationsIndexer) tutorial!
+
+  :::
+
+  Now the model has their relations indexed, we can safely create the corresponding tree. 💪
+*/
 
 const [relationsTree] = CUI.tables.relationsTree({
   components,
   models: [],
 });
 
-relationsTree.expanded = true;
 relationsTree.preserveStructureOnFilter = true;
+
+/* MD 
+  As you see, we've passed an empty models list because in the first place there are no models. However, the Relations Tree updates it-self each time a new model comes into the scene! Which makes it really handy to work with.
+
+  Great! As we already created the Relations Tree instance, let's add it to the HTML page. For it, let's create simple BIM panel component where we include the tree and also a pre-made IFC load button 👇
+*/
 
 const panel = BUI.Component.create(() => {
   const [loadIfcBtn] = CUI.buttons.loadIfc({ components });
@@ -75,6 +104,10 @@ const panel = BUI.Component.create(() => {
   `;
 });
 
+/* MD
+  Finally, let's append the BIM Panel to the page to see the Relations Tree working 😉
+  */
+
 const app = document.getElementById("app") as BUI.Grid;
 app.layouts = {
   main: {
@@ -87,3 +120,7 @@ app.layouts = {
 };
 
 app.layout = "main";
+
+/* MD
+  Congratulations! You've now a ready to go user interface that let's you show your model tree. 🥳
+  */
