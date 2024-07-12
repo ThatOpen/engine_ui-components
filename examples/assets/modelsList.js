daf8b4753e8852a3daec74dbfda3120eec4482bf
Juan Hoyos <juan.hoyos4@gmail.com>
2024-07-12 12:26:34 -0500

    chore: builds examples

--- examples/assets/modelsList.js
+++ examples/assets/modelsList.js
@@ -1,10 +1,10 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import{m,p as c,C as d,s as l,i as p,H as b,d as u,R as w,u as f,t as g,h as C}from"./template-1aA_fGv-.js";import{b as L}from"./index-D0HARS57.js";import{t as v}from"./index-CEeB810x.js";m.init();const e=new c,I=e.get(d),t=I.create(),s=new l(e);s.setup();t.scene=s;const a=document.createElement("bim-viewport"),i=new p(e,a);t.renderer=i;const r=new b(e);t.camera=r;a.addEventListener("resize",()=>{i.resize(),r.updateAspect()});const y=e.get(u);y.create(t);e.init();const E=e.get(w);await E.setup();const M=e.get(f);M.onFragmentsLoaded.add(n=>{t.scene&&t.scene.three.add(n)});const[h]=v.modelsList({components:e}),z=g.create(()=>{const[n]=L.loadIfc({components:e});return C`
+import"./modulepreload-polyfill-B5Qt9EMX.js";import{m,p as c,C as d,s as l,i as p,H as b,d as g,R as w,h as f,t as u,g as C}from"./template-DWsy8vKF.js";import{b as L}from"./index-BNlHQz7S.js";import{t as v}from"./index-DpvWeUsp.js";m.init();const e=new c,I=e.get(d),t=I.create(),s=new l(e);s.setup();t.scene=s;const a=document.createElement("bim-viewport"),i=new p(e,a);t.renderer=i;const r=new b(e);t.camera=r;a.addEventListener("resize",()=>{i.resize(),r.updateAspect()});const h=e.get(g);h.create(t);e.init();const y=e.get(w);await y.setup();const E=e.get(f);E.onFragmentsLoaded.add(n=>{t.scene&&t.scene.three.add(n)});const[M]=v.modelsList({components:e}),z=u.create(()=>{const[n]=L.loadIfc({components:e});return C`
    <bim-panel label="IFC Models">
     <bim-panel-section label="Importing">
       ${n}
     </bim-panel-section>
     <bim-panel-section icon="mage:box-3d-fill" label="Loaded Models">
-      ${h}
+      ${M}
     </bim-panel-section>
    </bim-panel> 
   `}),o=document.createElement("bim-grid");o.layouts={main:{template:`
