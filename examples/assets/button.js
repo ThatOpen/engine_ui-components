4f354afe946fe590722e935710cda988f2cf13fb
Juan Hoyos <juan.hoyos4@gmail.com>
2024-08-13 19:29:00 -0500

    chore: build examples

--- examples/assets/button.js
+++ examples/assets/button.js
@@ -1,1 +1,1 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import{M as e}from"./index-B56l1KZI.js";import"./state-QpXUsnxS.js";e.init();const t=document.body.querySelector("bim-button");t.addEventListener("click",()=>{alert("Your schedule has been created!")});
+import"./modulepreload-polyfill-B5Qt9EMX.js";import{M as e}from"./index-Bgl-j1gb.js";import"./state-QpXUsnxS.js";e.init();const t=document.body.querySelector("bim-button");t.addEventListener("click",()=>{alert("Your schedule has been created!")});
--- examples/assets/label.js
+++ examples/assets/label.js
@@ -1,1 +1,1 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import{M as i}from"./index-B56l1KZI.js";import"./state-QpXUsnxS.js";i.init();
+import"./modulepreload-polyfill-B5Qt9EMX.js";import{M as i}from"./index-Bgl-j1gb.js";import"./state-QpXUsnxS.js";i.init();
