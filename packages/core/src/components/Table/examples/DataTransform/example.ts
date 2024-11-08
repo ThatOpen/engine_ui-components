5684b1d9ba24b0c5c20bc82d7370f99c451d1d30
Juan Hoyos <juan.hoyos4@gmail.com>
2024-11-08 11:12:39 -0500

    sample structuring

---
+++ packages/core/src/components/Table/examples/DataTransform/example.ts
@@ -0,0 +1,12 @@
+/* MD
+  ### Using Custom HTML In Cells
+  ---
+  What about this?
+  */
+
+// A new comment right here
+console.log("Some code here...");
+
+/* MD
+  Congrats! You're the best.
+  */
