1444474d4e1db13e94ddcaa603c5d9b0b47edc45
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2025-03-15 14:24:20 +0300

    feat(core): animate bim-panel selection hide/show
    Note: the bim-label had its overflow set to hidden which resulted in bugs in teh selection dropdown animation if the label was put alone. I entirly deleted it and tested the app if any bug is presented in the tables, grids, taps, and found nothing.

--- packages/core/src/components/Label/index.ts
+++ packages/core/src/components/Label/index.ts
@@ -14,7 +14,6 @@ export class Label extends LitElement {
       --bim-icon--c: var(--bim-label--c);
       color: var(--bim-label--c, var(--bim-ui_bg-contrast-60));
       font-size: var(--bim-label--fz, var(--bim-ui_size-xs));
-      overflow: hidden;
       display: block;
       white-space: nowrap;
       line-height: 1.1rem;
