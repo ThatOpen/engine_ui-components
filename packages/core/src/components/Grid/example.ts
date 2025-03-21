82df1371b07fa75596f5fedd6fd63378e98214b7
Juan Hoyos <juan.hoyos4@gmail.com>
2025-03-21 17:55:10 -0500

    chore: update grid example

--- packages/core/src/components/Grid/example.ts
+++ packages/core/src/components/Grid/example.ts
@@ -31,14 +31,25 @@ const leftPanel = BUI.Component.create<BUI.Panel>(() => {
 });
 
 const ribbon = BUI.Component.create<BUI.Tabs>(() => {
-  // eslint-disable-next-line no-restricted-globals
-  const onThatOpenPeopleClick = () => open("https://people.thatopen.com/home");
+  const onThatOpenPeopleClick = () =>
+    window.open("https://people.thatopen.com/home");
+
+  const onToggleThemeClick = () => {
+    const html = document.querySelector("html");
+    if (!html) return;
+    if (html.classList.contains("bim-ui-dark")) {
+      html.classList.replace("bim-ui-dark", "bim-ui-light");
+    } else if (html.classList.contains("bim-ui-light")) {
+      html.classList.replace("bim-ui-light", "bim-ui-dark");
+    }
+  };
 
   return BUI.html`
    <bim-tabs>
     <bim-tab label="Toolbar A">
       <bim-toolbar>
         <bim-toolbar-section label="Some section">
+          <bim-button @click=${onToggleThemeClick} label="Toggle Theme" vertical icon="proicons:dark-theme"></bim-button>
           <bim-button label="Home" vertical icon="ic:round-home"></bim-button>
           <bim-button label="That Open People" vertical icon="eva:people-fill" @click=${onThatOpenPeopleClick}></bim-button>
           <bim-toolbar-group>
@@ -128,6 +139,45 @@ const bottomPanel = BUI.Component.create<BUI.Panel>(() => {
           DueDate: "2024-05-20",
           Status: "In Progress",
         },
+        children: [
+          {
+            data: {
+              Task: "Code Review - Round 1",
+              Description: "First pass code review",
+              AssignedTo: "Alice",
+              DueDate: "2024-05-18",
+              Status: "Completed",
+            },
+          },
+          {
+            data: {
+              Task: "Address Comments",
+              Description:
+                "Address the comments from the first round of review",
+              AssignedTo: "Author of Code",
+              DueDate: "2024-05-19",
+              Status: "In Progress",
+            },
+          },
+          {
+            data: {
+              Task: "Code Review - Round 2",
+              Description: "Second pass code review after addressing comments",
+              AssignedTo: "Alice",
+              DueDate: "2024-05-20",
+              Status: "Pending",
+            },
+          },
+          {
+            data: {
+              Task: "Merge Code",
+              Description: "Merge reviewed code after final approval",
+              AssignedTo: "Project Lead",
+              DueDate: "2024-05-21",
+              Status: "Not Started",
+            },
+          },
+        ],
       },
       {
         data: {
@@ -146,6 +196,55 @@ const bottomPanel = BUI.Component.create<BUI.Panel>(() => {
           DueDate: "2024-05-22",
           Status: "Completed",
         },
+        children: [
+          {
+            data: {
+              Task: "Create Test Cases",
+              Description: "Identify and document all necessary test cases",
+              AssignedTo: "Charlie",
+              DueDate: "2024-05-19",
+              Status: "Completed",
+            },
+          },
+          {
+            data: {
+              Task: "Implement Unit Tests",
+              Description:
+                "Write the actual unit tests based on the test cases",
+              AssignedTo: "Charlie",
+              DueDate: "2024-05-20",
+              Status: "Completed",
+            },
+          },
+          {
+            data: {
+              Task: "Run Unit Tests",
+              Description: "Execute the unit tests and analyze the results",
+              AssignedTo: "Charlie",
+              DueDate: "2024-05-21",
+              Status: "Completed",
+            },
+          },
+          {
+            data: {
+              Task: "Fix Failing Tests",
+              Description:
+                "Address any failing unit tests and correct the code",
+              AssignedTo: "Charlie",
+              DueDate: "2024-05-22",
+              Status: "Completed",
+            },
+          },
+          {
+            data: {
+              Task: "Refactor Code",
+              Description: "Improve the code structure and readability",
+              AssignedTo: "Charlie",
+              DueDate: "2024-05-23",
+              Status: "Completed",
+            },
+          },
+        ],
       },
       {
         data: {
@@ -155,6 +254,56 @@ const bottomPanel = BUI.Component.create<BUI.Panel>(() => {
           DueDate: "2024-05-18",
           Status: "Pending",
         },
+        children: [
+          {
+            data: {
+              Task: "Prepare Build Package",
+              Description: "Create the deployable package for the application",
+              AssignedTo: "David",
+              DueDate: "2024-05-16",
+              Status: "Completed",
+            },
+          },
+          {
+            data: {
+              Task: "Configure Staging Environment",
+              Description:
+                "Set up the staging environment with necessary configurations",
+              AssignedTo: "David",
+              DueDate: "2024-05-17",
+              Status: "Completed",
+            },
+          },
+          {
+            data: {
+              Task: "Upload Build Package",
+              Description: "Upload the build package to the staging server",
+              AssignedTo: "David",
+              DueDate: "2024-05-18",
+              Status: "Completed",
+            },
+          },
+          {
+            data: {
+              Task: "Run Deployment Script",
+              Description:
+                "Execute the deployment script to deploy the application",
+              AssignedTo: "David",
+              DueDate: "2024-05-18",
+              Status: "In Progress",
+            },
+          },
+          {
+            data: {
+              Task: "Verify Deployment",
+              Description:
+                "Check the application in the staging environment to ensure proper deployment",
+              AssignedTo: "David",
+              DueDate: "2024-05-19",
+              Status: "Pending",
+            },
+          },
+        ],
       },
       {
         data: {
@@ -229,17 +378,112 @@ const rightPanel = BUI.Component.create<BUI.Panel>(() => {
   return BUI.html`
     <bim-panel>
       <bim-panel-section label="Panel Section" icon="solar:settings-bold">
-        <bim-button @click=${onBtnClick} label="Click me!"></bim-button>
+        <bim-button label="Button With Nestings">
+          <bim-context-menu>
+            <bim-button label="Nested Button A" tooltip-title="asdasdasd" tooltip-text="asdasdasd">
+              <bim-context-menu>
+                <bim-button label="Nested Button A-1"></bim-button>
+                <bim-button label="Nested Button A-2"></bim-button>
+              </bim-context-menu>
+            </bim-button>
+            <bim-button label="Nested Button B"></bim-button>
+            <bim-button @click=${onBtnClick} label="Click me!"></bim-button>
+          </bim-context-menu>
+        </bim-button>
         <bim-selector value="Option A">
           <bim-option label="Option A" checked></bim-option>
           <bim-option label="Option B"></bim-option>
+          <bim-option label="Option C"></bim-option>
         </bim-selector>
       </bim-panel-section>
     </bim-panel>
   `;
 });
 
-const viewport = document.createElement("bim-viewport");
+const viewport = BUI.Component.create<BUI.Viewport>(() => {
+  const bottomToolbar = BUI.Component.create(
+    () => BUI.html`
+    <bim-tabs floating style="justify-self: center; border-radius: 0.5rem;">
+      <bim-tab label="Import">
+        <bim-toolbar>
+          <bim-toolbar-section label="Open Formats">
+            <bim-button label="IFC"></bim-button>
+            <bim-button label="BCF"></bim-button>
+            <bim-button label="IDS"></bim-button>
+            <bim-button label="DXF"></bim-button>
+          </bim-toolbar-section>
+          <bim-toolbar-section label="External">
+            <bim-button label="RVT"></bim-button>
+            <bim-button label="PDF"></bim-button>
+            <bim-button label="DWG"></bim-button>
+          </bim-toolbar-section>
+        </bim-toolbar>
+      </bim-tab>
+      <bim-tab label="Elements">
+        <bim-toolbar>
+          <bim-toolbar-section label="Visibility">
+            <bim-button label="Show All"></bim-button>
+            <bim-button label="Hide All"></bim-button>
+            <bim-button label="Hide In View"></bim-button>
+          </bim-toolbar-section>
+          <bim-toolbar-section label="Selection">
+            <bim-button label="All"></bim-button>
+            <bim-button label="Deselect"></bim-button>
+            <bim-button label="In View"></bim-button>
+          </bim-toolbar-section>
+        </bim-toolbar>
+      </bim-tab>
+      <bim-tab label="Measurement">
+        <bim-toolbar>
+          <bim-toolbar-section label="Tools">
+            <bim-button label="Area"></bim-button>
+            <bim-button label="Angle"></bim-button>
+            <bim-button label="Volume"></bim-button>
+            <bim-button label="Laser"></bim-button>
+            <bim-button label="Edge"></bim-button>
+            <bim-button label="Face"></bim-button>
+          </bim-toolbar-section>
+        </bim-toolbar>      
+      </bim-tab>
+    </bim-tabs>
+  `,
+  );
+
+  const rightToolbar = BUI.Component.create(
+    () => BUI.html`
+    <bim-toolbar vertical style="align-self: start;">
+      <bim-toolbar-section>
+        <bim-button icon="solar:cursor-bold" tooltip-title="Element Selection" tooltip-text="💡 Change the selection mode to individual elements"></bim-button>
+        <bim-button icon="carbon:area"></bim-button>
+        <bim-button icon="oui:vis-area-stacked"></bim-button>
+      </bim-toolbar-section>
+      <bim-toolbar-section>
+        <bim-button icon="ix:element-filled"></bim-button>
+        <bim-button icon="material-symbols:table"></bim-button>
+      </bim-toolbar-section>
+    </bim-toolbar>
+  `,
+  );
+
+  const grid = document.createElement("bim-grid");
+  grid.floating = true;
+  grid.layouts = {
+    main: {
+      template: `
+      "empty rightToolbar" 1fr
+      "bottomToolbar bottomToolbar" auto
+      /1fr
+    `,
+      elements: { bottomToolbar, rightToolbar },
+    },
+  };
+
+  grid.layout = "main";
+
+  return BUI.html`
+    <bim-viewport>${grid}</bim-viewport>
+  `;
+});
 
 type LayoutElements = {
   main: ["header", "sidebar", "content"];
@@ -276,8 +520,8 @@ grid.layouts = {
   app: {
     template: `
       "ribbon ribbon ribbon" auto
-      "leftPanel viewport rightPanel" 2fr
-      "leftPanel bottomPanel bottomPanel" 1fr
+      "leftPanel viewport rightPanel" 1fr
+      "leftPanel bottomPanel bottomPanel" 1.25fr
       / 22rem 1fr 20rem
     `,
     elements: {
