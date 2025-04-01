604a4b9991be47147eb6bc8f8407dbc01db62ac4
GoulielmosFloros <82643379+GoulielmosFloros@users.noreply.github.com>
2025-04-01 20:29:18 +0100

    fix: bim-icon svg image position & bim-label font-size/line height #56. (#67)
    * fix: bim-icon scg image position & bim-label font-size/line height #56.
    
    For this one #56 I fixed the initial bug for the svg image by adding display: flex; in the style of packages\core\src\components\Icon.ts.
    
    I also fixed a related issue with regards to the bim-label size and line height with ref #55, by changing line-height from 1 rem to 1em, from packages\core\src\components\Label.ts style. We could also remove the property entirely and it would work.
    
    To test it you can go to the Label html file change the font-size and the text and see that the icon will always be on the same place and the line-height now adjusts to the size.
    
    Reviewed-by: Goulielmos Floros
    Ref: #56
    
    * fix: updates on examples
    
    * update examples

--- examples/assets/classificationsTree.js
+++ examples/assets/classificationsTree.js
@@ -1,13 +1,13 @@
-import{x as l}from"./lit-element-CToom8Wf.js";import{P as p,C as d,W as f,S as b,a as g,b as w,G as y,I as u,F as C,r as T,T as v}from"./index-DNiVoluI.js";import{b as E}from"./index-CqhcBaEU.js";import{t as I}from"./index-kdXv3cq4.js";p.init();const e=new d,t=document.createElement("bim-viewport");t.name="viewer";const L=e.get(f),n=L.create(),o=new b(e);o.setup();n.scene=o;const r=new g(e,t);n.renderer=r;const c=new w(e);n.camera=c;t.addEventListener("resize",()=>{r.resize(),c.updateAspect()});const S=e.get(y);S.create(n);e.init();const F=e.get(u);await F.setup();const m=e.get(C);m.onFragmentsLoaded.add(s=>{n.scene&&n.scene.three.add(s)});const[P,G]=I.classificationTree({components:e,classifications:[]}),i=e.get(T);m.onFragmentsLoaded.add(async s=>{i.byEntity(s),await i.byPredefinedType(s),G({classifications:[{system:"entities",label:"Entities"},{system:"predefinedTypes",label:"Predefined Types"}]})});const z=v.create(()=>{const[s]=E.loadIfc({components:e});return l`
+import"./modulepreload-polyfill-B5Qt9EMX.js";import{T as l,C as p,W as d,S as f,a as b,b as g,G as w,I as y,F as u,t as C,l as T,m as v}from"./index-N2x2mgv3.js";import{b as L}from"./index-DAXFTtA6.js";import{t as E}from"./index-BazQpUSZ.js";import"./lit-html-paDGiEfB.js";l.init();const e=new p,n=document.createElement("bim-viewport");n.name="viewer";const I=e.get(d),t=I.create(),o=new f(e);o.setup();t.scene=o;const r=new b(e,n);t.renderer=r;const c=new g(e);t.camera=c;n.addEventListener("resize",()=>{r.resize(),c.updateAspect()});const S=e.get(w);S.create(t);e.init();const F=e.get(y);await F.setup();const m=e.get(u);m.onFragmentsLoaded.add(s=>{t.scene&&t.scene.three.add(s)});const[G,z]=E.classificationTree({components:e,classifications:[]}),i=e.get(C);m.onFragmentsLoaded.add(async s=>{i.byEntity(s),await i.byPredefinedType(s),z({classifications:[{system:"entities",label:"Entities"},{system:"predefinedTypes",label:"Predefined Types"}]})});const M=T.create(()=>{const[s]=L.loadIfc({components:e});return v`
    <bim-panel label="Classifications Tree">
     <bim-panel-section label="Importing">
       ${s}
     </bim-panel-section>
     <bim-panel-section label="Classifications">
-      ${P}
+      ${G}
     </bim-panel-section>
    </bim-panel> 
   `}),a=document.createElement("bim-grid");a.layouts={main:{template:`
       "panel viewport"
       / 23rem 1fr
-    `,elements:{panel:z,viewport:t}}};a.layout="main";document.body.append(a);
+    `,elements:{panel:M,viewport:n}}};a.layout="main";document.body.append(a);
--- examples/assets/textInput.js
+++ examples/assets/textInput.js
@@ -1,1 +1,1 @@
-import"./lit-element-CToom8Wf.js";import{M as e}from"./index-BCngLrVF.js";import"./state-H9kisBvQ.js";import"./ref-CyNRy2Tn.js";e.init();const t=document.querySelector("bim-text-input[name='name']");t.addEventListener("input",()=>{console.log(t.value)});
+import"./modulepreload-polyfill-B5Qt9EMX.js";import{M as e}from"./index-CcrzIsUO.js";import"./state-DYefyXr3.js";import"./lit-html-paDGiEfB.js";import"./ref-CInB0H-f.js";e.init();const t=document.querySelector("bim-text-input[name='name']");t.addEventListener("input",()=>{console.log(t.value)});
