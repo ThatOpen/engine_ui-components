92adf9e7454a0485a5f78e37e1cdfce1c1bdf4b8
Juan Hoyos <juan.hoyos4@gmail.com>
2026-07-06 21:34:56 -0500

    chore: update Tooltip example

--- packages/core/src/components/Tooltip/example.ts
+++ packages/core/src/components/Tooltip/example.ts
@@ -1,3 +1,9 @@
 import * as BUI from "../..";
 
 BUI.Manager.init();
+
+// --- visible setter ---
+const progTooltip = document.getElementById("prog-tooltip") as BUI.Tooltip;
+document.getElementById("btn-toggle-visible")?.addEventListener("click", () => {
+  progTooltip.visible = !progTooltip.visible;
+});
